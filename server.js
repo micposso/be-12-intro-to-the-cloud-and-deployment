2d2ba77309d8b25c5136a9b7b35e45145a641577
micposso <micposso@gmail.com>
2022-09-06 00:18:03 -0400

    add more configuration to json file

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
-  res.send('Hello, world! again...')
+  res.send('Hello, world!')
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
